0d3f4a373dc155aae2db65caaea3a9af935cc56b
kcdove <kayojukwu@gmail.com>
2022-12-05 11:58:37 +0100

    the players

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 # creating the board
 # total_number_of_seeds = 48
 #  total_number_of_pits = 12
@@ -5,9 +6,26 @@ pitseeds = [ 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0] # this is an array of the
 
 playing = True
 
+Player1_pit = ('a', 'b', 'c', 'd', 'e', 'f')
+player2_pit = ('g', 'h', 'i', 'j', 'k', 'l')
+
 while(playing):
-    i = 0
-    for element in pitseeds:
+    # a tuple of the player's pits
+    player1 = Player1_pit
+    if player1:
+        say = ("player1s' Turn. chose from g-l")
+    else:
+        say =("invalid pit")
+    player2 = player2_pit
+    if player2:
+        say = ("player2s' Turn. chose from g-l")
+    else:
+        say =("invalid pit")
+    print(say)
+    
+   
+    i = 0          #index variable
+    for seed in pitseeds:
         pitseeds[i] = int(pitseeds[i] )
         if int(pitseeds[i]) < 10:
             pitseeds[i] = " " + str(pitseeds[i])
@@ -15,13 +33,26 @@ while(playing):
             pitseeds[i] = str()
         i = i + 1
     print("")
+    if player2:
+        print("        l    k    j    i    h    g")
     print("+----+----+----+----+----+----+----+----+")
     print("|    | "+ pitseeds[12] +" | "+ pitseeds[11] +" | "+ pitseeds[10] +" | "+ pitseeds[9] +" | "+ pitseeds[8] +" | "+ pitseeds[7] +" |    |")
     print("| "+ pitseeds[13] +" |----+----+----+----+----+----| "+ pitseeds[6] +" |")
-    print("|    | "+ pitseeds[0] +" | "+ pitseeds[1] +" | "+ pitseeds[2] +" | "+ pitseeds[3] +" | "+ pitseeds[4] +" |"+ pitseeds[5] +"  |    |")
+    print("|    | "+ pitseeds[0] +" | "+ pitseeds[1] +" | "+ pitseeds[2] +" | "+ pitseeds[3] +" | "+ pitseeds[4] +" | "+ pitseeds[5] +" |    |")
     print("+----+----+----+----+----+----+----+----+")
-    print("")
+    
+    
+    if player1:
+        print("        a    b    c    d    e    f")
+        print("")
     userInput = input ("Enter 'q' to QUIT the game: ")
     
     if userInput == "q":
-        playing = False   
+        playing = False
+        
+#  how to pick the seed
+# how to distribute the seed
+# assign the players
+# how players take turns
+#  make sure the player select a valid pit
+# how to determine who wins 
