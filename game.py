c95a2aa81d9b37d01b33a264959584b968051d41
Aduwele <114133250+annah-belle@users.noreply.github.com>
2022-12-07 14:05:08 +0100

    More updates
    Lesson with Jannis

--- game.py
+++ game.py
@@ -8,6 +8,8 @@ playing = True
 
 player1 = True
 
+msgCode = 0
+
 #Player1_pit = ('a', 'b', 'c', 'd', 'e', 'f')
 #player2_pit = ('g', 'h', 'i', 'j', 'k', 'l')
 
@@ -27,13 +29,26 @@ while(playing):
     
     
     msg = ""
-    if player1:
+    if player1 and msgCode == 0:
         msg = "Player One's turn, choose from a-f"
-    else:
+    elif not(player1) and msgCode == 0:
         msg = "Player Two's turn, choose from g-l"
+    elif player1 and msgCode == -2:
+        msg = "Invalid Input. Try again Player One"
+    elif not(player1) and msgCode == -2:
+        msg = "Invalid Input. Try again Player Two"
+    elif player1 and msgCode == -1:
+        msg = "You must choose a non-empty bin, Player One"
+    elif not(player1) and msgCode == -1:
+        msg = "You must choose a non-empty bin, Player Two"
+    elif player1 and msgCode == -3:
+        msg = "Invalid Input. Try again Player One"
+    elif not(player1) and msgCode == -3:
+        msg = "Invalid Input. Try again Player Two"
     print("")
     print(msg)
-    print("")    
+    print("") 
+    msgCode = 0   
     
    
     i = 0          #index variable
@@ -47,8 +62,8 @@ while(playing):
         #end of the loop
         
     print("")
-    if not(player1):
-        print("        l    k    j    i    h    g")
+    
+    print("        l    k    j    i    h    g")
     print("+----+----+----+----+----+----+----+----+")
     print("|    | "+ pitseeds[12] +" | "+ pitseeds[11] +" | "+ pitseeds[10] +" | "+ pitseeds[9] +" | "+ pitseeds[8] +" | "+ pitseeds[7] +" |    |")
     print("| "+ pitseeds[13] +" |----+----+----+----+----+----| "+ pitseeds[6] +" |")
@@ -56,12 +71,11 @@ while(playing):
     print("+----+----+----+----+----+----+----+----+")
     
     
-    if player1:
-        print("        a    b    c    d    e    f")
-        print("")
-    userInput = input ("Enter 'q' to QUIT the game: ")
     
-    selectedBin = 0
+    print("        a    b    c    d    e    f")
+    print("")
+    userInput = input ("Enter a letter to choose a bin or 'q' to QUIT the game: ")
+    
     if userInput == "q":
         playing = False
     elif player1 and userInput == "a":
@@ -88,12 +102,57 @@ while(playing):
         selectedBin = 8
     elif not(player1) and userInput == "g":
         selectedBin = 7
+    else:
+        selectedBin = -2
+        msgCode = -2  #invalid input
+       
+   
+ 
+    if int(selectedBin) >= 0:
+        takeawayPile = pitseeds[selectedBin]  
         
-    pitseeds [selectedBin] = 0
-    
-    
-    
-    player1 = not(player1)
+        if int(takeawayPile) > 0:
+            
+            ######## INSTEAD OF THIS PART BELOW WE NEED A LOGIC THAT MOVES OUR PILES CLOCKWISE
+            userInput = input ("Enter a letter to distribute seeds or 'q' to QUIT the game: ")
+            
+            if userInput == "q":
+                playing = False
+            elif not(player1) and userInput == "a":
+                selectedBin2 = 0
+            elif not(player1) and userInput == "b":
+                selectedBin2 = 1
+            elif not(player1) and userInput == "c":
+                selectedBin2 = 2
+            elif not(player1) and userInput == "d":
+                selectedBin2 = 3
+            elif not(player1) and userInput == "e":
+                selectedBin2 = 4
+            elif not(player1) and userInput == "f":
+                selectedBin2 = 5
+            elif player1 and userInput == "l":
+                selectedBin2 = 12
+            elif player1 and userInput == "k":
+                selectedBin2 = 11
+            elif player1 and userInput == "j":
+                selectedBin2 = 10
+            elif player1 and userInput == "i":
+                selectedBin2 = 9
+            elif player1 and userInput == "h":
+                selectedBin2 = 8
+            elif player1 and userInput == "g":
+                selectedBin2 = 7
+            else:
+                selectedBin2 = -3
+                msgCode = -3  #invalid input
+            
+                ###### UNTIL HERE
+            if selectedBin2 >= 0:
+                pitseeds[selectedBin2] = str( int(pitseeds[selectedBin2]) + int(takeawayPile))
+                pitseeds[selectedBin] = str(0)
+                player1 = not(player1)
+        else:
+            msgCode = -1 # empty bin was chosen
         
 #  how to pick the seed
 # how to distribute the seed
